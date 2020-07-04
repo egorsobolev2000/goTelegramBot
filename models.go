cad9ecddd25b0f9e8f67b61f7408cd4910ca236b
egorsobolev2000 <web-disain@mail.ua>
2020-07-04 16:04:42 +0300

    Добавил новые модели: BotCommand, ReplyKeyboardMarkup, KeyboardButton, KeyboardButtonPollType.

--- models.go
+++ models.go
@@ -4,12 +4,18 @@ package goTelegramBot
 type Update struct {
 	UpdateId int 					`json:"update_id"`
 	Message Message					`json:"message"`
+	EditMessage Message				`json:"edited_message"`
+}
+
+type BotCommand struct {
+	Command string					`json:"command"`					// Текст команды, 1-32 символа. Может содержать только строчные английские буквы, цифры и подчеркивания.
+	Description string				`json:"description"`				// Описание команды, 3-256 символов
 }
 
 type Message struct {
 	Chat Chat						`json:"chat"`
 	Text string						`json:"text"`
-	UserName User					`json:"from"`
+	UserInfo User					`json:"from"`
 }
 
 type Chat struct {
@@ -36,3 +42,22 @@ type User struct {
 	CanReadAllGroupMessage bool		`json:"can_read_all_group_messages"`
 	SupportsInlineQueries bool		`json:"supports_inline_queries"`
 }
+
+type ReplyKeyboardMarkup struct {
+	Keyboard []KeyboardButton		`json:"keyboard"`
+	ResizeKeyboard bool				`json:"resize_keyboard"`
+	OneTimeKeyBoard bool			`json:"one_time_keyboard"`
+	Selective bool					`json:"selective"`
+}
+
+type KeyboardButton struct {
+	Text string							`json:"text"`
+	RequestContact bool					`json:"request_contact"`
+	RequestLocation bool				`json:"request_location"`
+	RequestPoll KeyboardButtonPollType	`json:"request_poll"`
+}
+
+type KeyboardButtonPollType struct {
+	Type string 					`json:"type"`
+}
+
