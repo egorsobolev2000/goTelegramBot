d9463eb0e6c69e7eb877e2a71545adb911ed1c38
egorsobolev2000 <web-disain@mail.ua>
2020-07-04 04:02:07 +0300

    A

--- chat.go
+++ chat.go
@@ -1,7 +1,25 @@
 package goTelegramBot
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	gtb "github.com/egorsobolev2000/goTelegramBot"
+	"net/http"
+)
 
-func SomeSecondTest(v string)  {
-	fmt.Printf("Some prirnt %v\n", v)
+func SendMessage(update gtb.Update, replayText string, botUrl string) error {
+	var botMessage gtb.BotMessage
+	botMessage.ChatId = update.Message.Chat.ChatId
+	botMessage.Text = replayText
+	buf, err := json.Marshal(botMessage)
+	if err != nil {
+		return err
+	}
+
+	_, err = http.Post(botUrl + "/sendMessage", "application/json", bytes.NewBuffer(buf))
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
