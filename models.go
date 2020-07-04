7f1da029138c14e963b7878e145810d1da179729
egorsobolev2000 <web-disain@mail.ua>
2020-07-04 03:46:12 +0300

    A

--- models.go
+++ models.go
@@ -1,11 +1,25 @@
 package goTelegramBot
 
-import "fmt"
 
-func SomeTest(v string)  {
-	fmt.Printf("Some print %v\n", v)
+type Update struct {
+	UpdateId int 		`json:"update_id"`
+	Message Message		`json:"message"`
 }
 
-func LAstTest()  {
-	fmt.Print("4")
+type Message struct {
+	Chat Chat			`json:"chat"`
+	Text string			`json:"text"`
+}
+
+type Chat struct {
+	ChatId int			`json:"id"`
+}
+
+type RestResponse struct {
+	Result []Update		`json:"result"`
+}
+
+type BotMessage struct {
+	ChatId int			`json:"chat_id"`
+	Text string			`json:"text"`
 }
